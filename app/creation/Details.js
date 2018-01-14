cd0131c64411458b2799df7e642c760f22d92483
chengong <654450188@qq.com>
2018-01-14 10:52:39 +0800

    视频出错提示，视频结束重新播放功能

--- app/creation/Details.js
+++ app/creation/Details.js
@@ -87,7 +87,7 @@ export default class Details extends Component {
   }
 
   static _replay(){
-    this.refs.videoPlayer.seek(20);  //视频重新播放
+    this.refs.videoPlayer.seek(0);  //视频重新播放
   }
 
   render() {
