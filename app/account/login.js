87e9d10c5b6ef4bad622c7bffa9f2abcc5ba018b
chengong <654450188@qq.com>
2018-01-18 01:41:29 +0800

    倒计时 && 验证码+手机号 登录

--- app/account/login.js
+++ app/account/login.js
@@ -11,12 +11,15 @@ import Button from 'react-native-button';
 import request from '../common/request';
 import url from '../common/url';
 
+let {CountDownText} = require('react-native-sk-countdown');
+
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       phoneNumber: '',
       verifyCode: '',
+      countingDone: false,
       codeSent: false
     };
   }
@@ -33,11 +36,39 @@ export default class Login extends Component {
     });
   }
 
+  /**
+   * 提交手机号和验证码
+   * @private
+   */
   static _submit() {
+    let {phoneNumber, verifyCode} = this.state;
+    if (!phoneNumber || !verifyCode) {
+      AlertIOS.alert('手机号或验证码不能为空');
+      return;
+    }
+    let body = {
+      phoneNumber,
+      verifyCode,
+    };
+    request.post(url.verify, body).then(res => {
+      if (res && res.success) {
+        console.log('login ok');
+        console.log(res);
 
+      } else {
+        AlertIOS.alert('获取验证码失败，请检查手机号');
+      }
+    }).catch(err => {
+      console.log(err);
+      AlertIOS.alert('获取验证码失败，请稍后再试');
+    });
   }
 
-  static _sendVerifyCodes() {
+  /**
+   * 提交手机号获取验证码
+   * @private
+   */
+  static _sendVerifyCode() {
     let phoneNumber = this.state.phoneNumber;
     if (!phoneNumber) {
       AlertIOS.alert('手机号不能为空');
@@ -49,6 +80,7 @@ export default class Login extends Component {
     request.post(url.signup, body).then(res => {
       if (res && res.success) {
         Login._showVerifyCode.call(this);
+
       } else {
         AlertIOS.alert('获取验证码失败，请检查手机号');
       }
@@ -56,13 +88,26 @@ export default class Login extends Component {
       console.log(err);
       AlertIOS.alert('获取验证码失败，请稍后再试');
     });
-
-
   }
 
+  /**
+   * 显示验证码
+   * @private
+   */
   static _showVerifyCode() {
     this.setState({
       codeSent: true,
+      countingDone: false,
+    });
+  }
+
+  /**
+   * 显示倒计时
+   * @private
+   */
+  static _countingDone(){
+    this.setState({
+      countingDone: true,
     });
   }
 
@@ -83,7 +128,7 @@ export default class Login extends Component {
 
           {
             this.state.codeSent ?
-              <View>
+              <View style={styles.verifyCodeBox}>
                 <TextInput
                   style={styles.inputField}
                   placeholder={'输入验证码'}
@@ -93,6 +138,28 @@ export default class Login extends Component {
                   onChangeText={Login._saveVerify.bind(this)}
                   autoFocus={true}
                 />
+
+                {
+                  this.state.countingDone
+                    ? <Button
+                      style={styles.countBtn}
+                      onPress={Login._sendVerifyCode.bind(this)}
+                    >
+                      获取验证码
+                    </Button>
+                    : <CountDownText
+                      style={styles.countBtn}
+                      countType='seconds' // 计时类型：seconds / date
+                      auto={true} // 自动开始
+                      afterEnd={Login._countingDone.bind(this)} // 结束回调
+                      timeLeft={10} // 正向计时 时间起点为0秒
+                      step={-1} // 计时步长，以秒为单位，正数则为正计时，负数为倒计时
+                      startText='获取验证码' // 开始的文本
+                      endText='获取验证码' // 结束的文本
+                      intervalText={(sec) => sec + '秒重新获取'} // 定时的文本回调
+                    />
+                }
+
               </View> : null
 
           }
@@ -100,7 +167,7 @@ export default class Login extends Component {
           {
             this.state.codeSent
               ? <Button style={styles.btn} onPress={Login._submit.bind(this)}>登录</Button>
-              : <Button style={styles.btn} onPress={Login._sendVerifyCodes.bind(this)}>获取验证码</Button>
+              : <Button style={styles.btn} onPress={Login._sendVerifyCode.bind(this)}>获取验证码</Button>
           }
         </View>
       </View>
@@ -135,6 +202,26 @@ const styles = StyleSheet.create({
     fontSize: 16,
     backgroundColor: '#fff',
     borderRadius: 4
+  },
+
+  verifyCodeBox:{
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  countBtn: {
+    width: 110,
+    height: 40,
+    padding: 10,
+    marginLeft: 8,
+    backgroundColor: '#ee735c',
+    borderColor: '#ee735c',
+    color: '#fff',
+    textAlign: 'left',
+    fontWeight: '600',
+    fontSize: 15,
+    borderRadius: 2,
   },
 
   btn: {
